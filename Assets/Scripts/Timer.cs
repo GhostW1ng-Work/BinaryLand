dfe2a7da2a9cf34a240866ba85c281016b4383e5
GhostW1ng <workjustghostw1ng@gmail.com>
2022-07-29 22:48:55 +0300

    Create Timer, UI

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float _maxTime;
+    [SerializeField] private TMP_Text _text;
+    [SerializeField] private PenguinMover[] _penguins;
+
+    private int _time;
+ 
+    private void Start()
+    {
+        _time = (int)_maxTime;
+        _text.text = _time.ToString();
+    }
+
+    private void Update()
+    {
+        _maxTime -= Time.deltaTime;
+        _time = (int)_maxTime;
+        _text.text = _time.ToString();
+
+        if(_maxTime <= 0)
+        {
+            _time = 0;
+            _text.text = _time.ToString();
+
+            StopPlayer();
+        }
+    }
+
+    private void StopPlayer()
+    {
+        for (int i = 0; i < _penguins.Length; i++)
+        {
+            _penguins[i].SetCanMove();
+            _penguins[i].SetCanAttack();
+            _penguins[i].SetIsGameOverTrue();
+        }
+    }
+}
